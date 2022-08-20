aaf384651800063871a551f969d49c9ebd6b6786
Mar10Josh <65928867+Mar10Josh@users.noreply.github.com>
2022-08-20 09:17:30 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'YouStyel'
+copyright = '2021, Josiah'
+author = 'Josiah'
 
 release = '0.1'
 version = '0.1.0'
